65cd52f7d9dcbc898f36df39d4c49abf6ab871ee
Jamie <jamielynjenks@gmail.com>
2020-11-30 14:49:01 -0700

    main content and image added

--- js/index.js
+++ js/index.js
@@ -49,11 +49,28 @@ let navATexts = Object.keys(siteContent['nav'])//make object keys into array
 //loop over array to add the text-content
 for(i=0; i < navATexts.length-1; i++){
   let key = navATexts[i]
-  console.log(key)
   navAList[i].textContent= siteContent['nav'][key];
 }
 
 //cta
 document.querySelector('h1').textContent = siteContent['cta']['h1'];
 document.querySelector('button').textContent = siteContent['cta']['button'];
-document.querySelector('#cta-img').src = siteContent['cta']['img-src']
+document.querySelector('#cta-img').src = siteContent['cta']['img-src'];
+
+//top-content
+let textKeys = ['features-content',"about-content","services-content","product-content","vision-content"];
+let texts = document.querySelectorAll('.text-content p');
+for(i=0; i<texts.length; i++){
+let contentKeys = textKeys[i];
+texts[i].textContent = siteContent['main-content'][contentKeys];
+}
+//h4 main content
+let headerKeys = ["features-h4", "about-h4", 'services-h4', 'product-h4', 'vision-h4'];
+let headers = document.querySelectorAll(".main-content .text-content h4");
+for(i=0;i<headers.length; i++){
+  let contentKeys = headerKeys[i];
+  headers[i].textContent = siteContent['main-content'][contentKeys];
+}
+
+//img main content
+document.querySelector('#middle-img').src = siteContent['main-content']['middle-img-src'];
